201d3fe826c6596dbefa98adead70cd18773db45
OACH-py <oach.core.sys@outlook.com>
2025-11-28 15:57:26 -0600

    feat: agregar juego de adivinar el número

---
+++ 01_Fundamentos/02_adivina_el_numero.py
@@ -0,0 +1,28 @@
+import random
+
+num_secreto = random.randint(1,100)
+intentos = 0 
+num_usuario = 0
+
+print("-"*40)
+print("El juego consite en adivinar el numero")
+print("-"*40)
+
+def pedir_num():
+    dato = int(input("ingresa un mumero:"))
+    return dato
+
+while num_usuario != num_secreto:
+    intentos += 1
+    try:
+        num_usuario = pedir_num()
+    except ValueError:
+        print("Error: Ingrese un numero entero")
+        pedir_num
+    
+    if num_usuario > num_secreto:
+        print("Mas bajo...")
+    elif num_usuario < num_secreto:
+        print("Mas alto...")
+
+print(f"¡Ganaste en {intentos} intentos!")
