d0236b2524206e35eb1f2e97336ba96747e2a677
OACH-py <oach.core.sys@outlook.com>
2025-11-27 23:29:22 -0600

    feat: add detailed bill summary and per-person payment output, and remove an emoji from an input prompt.

--- 01_Fundamentos/01_calculadora_propinas.py
+++ 01_Fundamentos/01_calculadora_propinas.py
@@ -4,13 +4,23 @@ def calculadora_propinas ():
     try:
         cuenta = float(input("💰 ¿Cuál fue el total de la cuenta?: $"))
         propina_pct = float(input("📈 ¿Qué % de propina deseas dar?: "))
-        personas = int(input("busts_in_silhouette ¿Entre cuántas personas se divide?: "))
-
+        personas = int(input("¿Entre cuántas personas se divide?: "))
+    
         if personas <= 0:
             print("Error: El numero de personas debe ser mayor a 0")
             return
         propina = cuenta * (propina_pct / 100)
         total = cuenta + propina 
         pago_persona = total / personas
+        
+        print("---Datos de la Cuenta---")
+        print("-"*30)
+        print(f"Subtotal:${cuenta:,.2f}")
+        print(f"Propina:${propina_pct:,.2f}")
+        print(f"Total:${total:,.2f}")
+        print("-"*30)
+        print(f"Cada uno paga:{pago_persona:,.2f}")
+
     except ValueError:
         print("❌ Error: Ingresa valores validos ")
+calculadora_propinas()
