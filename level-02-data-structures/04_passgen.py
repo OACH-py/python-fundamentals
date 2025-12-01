f9384ed10581a849380ccec90c7685556e80a5d8
OACH-py <oach.core.sys@outlook.com>
2025-11-30 20:42:43 -0600

    feat: mejorar la entrada de longitud de contraseña con manejo de errores

--- level-02-data-structures/04_passgen.py
+++ level-02-data-structures/04_passgen.py
@@ -3,9 +3,16 @@ import string
 
 characters = string.ascii_letters + string.digits + string.punctuation
 print("Password Generator")
-print("-"*40)
-password_length = int(input("¿Qué tan larga quieres la contraseña?\n"))
-print("-"*40)
+while True:
+    try:
+        password_length = int(input("¿Qué tan larga quieres la contraseña? (solo números)\n"))
+        if password_length <= 0:
+            print("Error: La longitud debe ser un número positivo mayor que cero.")
+            continue # Volver al inicio del ciclo (sin ejecutar el 'break')
+        break
+    except ValueError:
+        print("Error: Ingresa un valor numérico válido.")
+
 password = []
 for i in range (password_length):
 
