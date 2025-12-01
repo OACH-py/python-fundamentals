397e508f568ed4dda4f48b3916f29ec1367d81b7
OACH-py <oach.core.sys@outlook.com>
2025-11-30 20:45:56 -0600

    feat: mejorar la presentación del generador de contraseñas con separadores

--- level-02-data-structures/04_passgen.py
+++ level-02-data-structures/04_passgen.py
@@ -2,15 +2,19 @@ import secrets
 import string
 
 characters = string.ascii_letters + string.digits + string.punctuation
+print("-"*30)
 print("Password Generator")
 while True:
     try:
+        print("-"*30)
         password_length = int(input("¿Qué tan larga quieres la contraseña? (solo números)\n"))
         if password_length <= 0:
+            print("-"*30)
             print("Error: La longitud debe ser un número positivo mayor que cero.")
             continue # Volver al inicio del ciclo (sin ejecutar el 'break')
         break
     except ValueError:
+        print("-"*30)
         print("Error: Ingresa un valor numérico válido.")
 
 password = []
@@ -20,5 +24,6 @@ for i in range (password_length):
     password.append(random_characters)
 
 password = "".join(password)
-
+print("-"*password_length)
 print(password)
+print("-"*password_length)
