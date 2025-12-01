3a5367163178438bb47faef8355a6f107a6f9fe3
OACH-py <oach.core.sys@outlook.com>
2025-11-30 20:22:05 -0600

    feat: actualizar generador de contraseñas para usar el módulo secrets

--- level-02-data-structures/04_passgen.py
+++ level-02-data-structures/04_passgen.py
@@ -1,8 +1,17 @@
-import random
+import secrets
 import string
 
-#experimentacion
-print(string.ascii_letters)
-print(string.digits)
-print(string.punctuation)
-print(string.printable)
+characters = string.ascii_letters + string.digits + string.punctuation
+print("Password Generator")
+print("-"*40)
+password_length = int(input("¿Qué tan larga quieres la contraseña?\n"))
+print("-"*40)
+password = []
+for i in range (password_length):
+
+    random_characters = secrets.choice(characters)
+    password.append(random_characters)
+
+password = "".join(password)
+
+print(password)
