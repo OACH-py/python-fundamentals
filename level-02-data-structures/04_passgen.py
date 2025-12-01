2348c03018e52b30f4243911b2ce05f97c1b8670
OACH-py <oach.core.sys@outlook.com>
2025-11-30 21:58:09 -0600

    feat: reestructurar la generación de contraseñas para mejorar la legibilidad y el flujo de control

--- level-02-data-structures/04_passgen.py
+++ level-02-data-structures/04_passgen.py
@@ -17,13 +17,30 @@ while True:
         print("-"*30)
         print("Error: Ingresa un valor numérico válido.")
 
-password = []
-for i in range (password_length):
+while True:
+    password = []
+    for i in range (password_length):
 
-    random_characters = secrets.choice(characters)
-    password.append(random_characters)
+        random_char = secrets.choice(characters)
+        password.append(random_char)
+        
+    password = "".join(password)
+    
+    has_letter = False
+    has_digit = False
+    has_symbol = False
 
-password = "".join(password)
+    for char in password:
+        if char in string.ascii_letters:
+            has_letter = True
+        if char in string.digits:
+            has_digit = True
+        if char in string.punctuation:
+            has_symbol = True
+    
+    if has_digit and has_letter and has_symbol:
+        break
+
 print("-"*password_length)
-print(password)
+print(f"Tu contraseña es:\n{password}")
 print("-"*password_length)
